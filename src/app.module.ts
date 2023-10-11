cb474fd1a488d18aa0302c26bb27a48df5fa03cc
cabrera-evil <douglascabrera.dev@gmail.com>
2023-10-10 18:02:08 -0700

    Fix: app module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfig } from './config/app.config';
 import { PackageModule } from './modules/package/package.module';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [AppConfig, PackageModule],
+  imports: [AppConfig, PackageModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
